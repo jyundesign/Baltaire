0e3583ddbe6a9c667a488c5492de6bc2e5f920ee
Salvatore Candela <salvatore@lappy.local>
2014-05-17 10:35:01 -0700

    first commit

---
+++ index.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+    <head>
+        <meta http-equiv="content-type" content="text/html; charset=windows-1252" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+        <title>Baltaire</title>
+		<script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.1/jquery.min.js"></script>
+		<link rel="stylesheet" href="./css/jquery.maximage.css?v=1.2" type="text/css" media="screen" charset="utf-8" />
+		<link type="text/css" rel="stylesheet" href="http://fast.fonts.net/cssapi/640aadb6-81e0-47b3-bb96-ae809f996626.css"/>
+		<link type="text/css" rel="stylesheet" href="style.css"/>
+</head>
+<body>
+	<div id="background-image">
+			<div id="maximage">
+				 <img src="img/splash-bkg.jpg" width="1600" height="1039"/>
+			</div>
+	</div>
+
+	<div id="content">
+		<div class="header">
+			<img src="img/splash-logo.png">
+			<h3>Opening Summer 2014</h3>
+		</div>
+		
+		<div class="description">
+			<h4>Classic meets contemporary. <br />
+			Lunch in the sun, dinner under the stars. <br />
+			Cocktails and camaraderie. <br />
+			The steakhouse reimagined.</h4>
+		</div>
+		<div class="bottom-container">
+			<div class="contact-info">
+				<a href="mailto:hello@baltaire.com"><p>hello@baltaire.com</p></a>
+				<p>11647 San Vicente Boulevard in Los Angeles </p>
+			</div>
+
+			<div class="sign-up">
+				<p>Stay Tuned</p>
+				<form action="http://baltaire.us8.list-manage2.com/subscribe/post?u=aed759eed0a05d745de419340&amp;id=f2af93aaef" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+	<input type="email" value="" name="EMAIL" class="email email-input" id="mce-EMAIL" placeholder="email address" required>
+    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+    <div style="position: absolute; left: -5000px;"><input type="text" name="b_aed759eed0a05d745de419340_f2af93aaef" tabindex="-1" value=""></div>
+    <div class="clear" style="display:inline;"><input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="submit-button"></div>
+				</form>
+			</div>
+		</div>
+	</div>
+<?php include('max-image.php'); ?>
+</body>
+</html>
+
